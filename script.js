70467f9eac1cf9fefd411b51d057e663c56d5a7b
VishaliSubramanium <43952038+VishaliSubramanium@users.noreply.github.com>
2026-09-05 20:15:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 
-// Create a promise with a random delay between 1 and 3 seconds
 function createPromise() {
   const time = Math.random() * 2 + 1;
 
@@ -12,25 +10,15 @@ function createPromise() {
   });
 }
 
-// Record the start time
-const startTime = performance.now();
-
-// Create 3 promises
 const promise1 = createPromise();
 const promise2 = createPromise();
 const promise3 = createPromise();
 
-// Wait until all promises are resolved
 Promise.all([promise1, promise2, promise3]).then((results) => {
-  const endTime = performance.now();
-
-  // Calculate total elapsed time
-  const totalTime = (endTime - startTime) / 1000;
-
-  // Remove Loading...
+  // Remove Loading... row
   output.innerHTML = "";
 
-  // Add promise results
+  // Add rows for each promise
   results.forEach((time, index) => {
     const row = document.createElement("tr");
 
@@ -42,12 +30,14 @@ Promise.all([promise1, promise2, promise3]).then((results) => {
     output.appendChild(row);
   });
 
-  // Add total row
+  // Total time = longest promise time
+  const totalTime = Math.max(...results);
+
   const totalRow = document.createElement("tr");
 
   totalRow.innerHTML = `
-    <th>Total</th>
-    <th>${totalTime.toFixed(3)}</th>
+    <td>Total</td>
+    <td>${totalTime.toFixed(3)}</td>
   `;
 
   output.appendChild(totalRow);
